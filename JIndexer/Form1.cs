7d55a5c41231441156bf6a8272c5732de2a52c9a
Juan Martinez <juan_martinez3@dellteam.com>
2019-07-14 21:32:47 -0500

    first commit

--- JIndexer/Form1.cs
+++ JIndexer/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,24 @@ namespace JIndexer
         public Form1()
         {
             InitializeComponent();
+
+            String[] files = Directory.GetFiles(@"C:\");
+            DataTable table = new DataTable();
+            table.Columns.Add(new DataColumn());
+            table.Rows.Add("File Name");
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                table.Rows.Add(files[i]);
+            }
+
+            dataGridView1.DataSource = table;
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
         }
     }
 }
